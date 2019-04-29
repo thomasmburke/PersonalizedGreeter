3df15c553a20d50d36f284f605a60b51eaad099a
thomasmburke <tburke@bu.edu>
2019-04-29 15:02:46 -0400

    calls rekognition module to create a collection

--- environment_setup/create_rekognition_collection.py
+++ environment_setup/create_rekognition_collection.py
@@ -1,4 +1,9 @@
-import boto3
-import json
+import logging
+import sys
+sys.path.insert(0, '../src')
+from rekognition_ops import RekognitionOps
 
-collectionId = 'FriendsCollection'
+if __name__ == '__main__':
+    # Set default logging level
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    RekognitionOps().create_collection()
