f72f2dc72d3eae834b0196d8b8f1634b7bf4eb61
Thomas Burke <40719837+thomasmburke@users.noreply.github.com>
2019-05-01 09:43:31 -0400

    added format to logger

--- environment_setup/create_rekognition_collection.py
+++ environment_setup/create_rekognition_collection.py
@@ -5,5 +5,9 @@ from rekognition_ops import RekognitionOps
 
 if __name__ == '__main__':
     # Set default logging level
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    logging.basicConfig(
+        stream=sys.stdout, 
+        level=logging.INFO, 
+        format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+        datefmt='%Y-%m-%dT%H:%M:%S')
     RekognitionOps().create_collection()
