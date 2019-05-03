b791f4e0c432186683cd6dee8ab2ffcf28efbc9e
thomasmburke <tburke@bu.edu>
2019-05-03 15:33:26 -0400

    adding functionality to extend a facebook token

--- environment_setup/facebook_scraper.py
+++
@@ -1,9 +0,0 @@
-import facebook
-
-graph = facebook.GraphAPI('USERACCESSTOKEN')
-app_id = '' # Obtained from https://developers.facebook.com/
-app_secret = '' # Obtained from https://developers.facebook.com/
-
-# Extend the expiration time of a valid OAuth access token.
-extended_token = graph.extend_access_token(app_id, app_secret)
-print (extended_token) #verify that it expires in 60 days
