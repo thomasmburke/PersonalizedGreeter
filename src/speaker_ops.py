3ef2282ba0bd1f91f06778780bf1658dc6ca24c7
Thomas Burke <tburke@bu.edu>
2019-05-04 17:09:33 -0400

    added missing selfs

--- src/speaker_ops.py
+++ src/speaker_ops.py
@@ -10,17 +10,17 @@ class SpeakerOps:
 
     # Convert boto3 audio stream to Bytes stream
     # for compatibility with pygame
-    def play_audio_stream(audioStream):
+    def play_audio_stream(self, audioStream):
         # PyGame initialization - upon module loading
         pygame.init() # may want to initialize once in the detector
         pygame.mixer.init() # may want to initialize once in the detector
         audio = io.BytesIO(audioStream.read())
-        play_audio(audio)
+        self.play_audio(audio)
         pygame.mixer.quit()
         pygame.quit()
 
     # Here we play the audio stream
-    def play_audio(audio):
+    def play_audio(self, audio):
         pygame.mixer.music.load(audio)
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy():
