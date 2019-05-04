d2afcd194423c120ec716f318d6b46350d06bb5e
Thomas Burke <tburke@bu.edu>
2019-05-04 16:42:50 -0400

    OOP for the speakers enabled

--- src/speaker_ops.py
+++ src/speaker_ops.py
@@ -1,19 +1,27 @@
 import io
 import pygame
 
-# PyGame initialization - upon module loading
-pygame.init()
-pygame.mixer.init()
+class SpeakerOps:
+    """
+    SpeakerOps: This module is responsible for playing the greeting stream
+    """
+    def __init__(self):
+        pass
 
-# Convert boto3 audio stream to Bytes stream
-# for compatibility with pygame
-def play_audio_stream(audio_stream):
-    audio = io.BytesIO(audio_stream.read())
-    play_audio(audio)
+    # Convert boto3 audio stream to Bytes stream
+    # for compatibility with pygame
+    def play_audio_stream(audioStream):
+        # PyGame initialization - upon module loading
+        pygame.init() # may want to initialize once in the detector
+        pygame.mixer.init() # may want to initialize once in the detector
+        audio = io.BytesIO(audioStream.read())
+        play_audio(audio)
+        pygame.mixer.quit()
+        pygame.quit()
 
-# Here we play the audio stream
-def play_audio(audio):
-    pygame.mixer.music.load(audio)
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy():
-        pygame.time.Clock().tick(10)
+    # Here we play the audio stream
+    def play_audio(audio):
+        pygame.mixer.music.load(audio)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
