10063799dc4ea1ed26f0c18b47c6db7387526abf
thomasmburke <tburke@bu.edu>
2019-05-04 14:25:52 -0400

    commands for pip installs on Rpi

---
+++ environment_setup/commands_before_pip_installs.sh
@@ -0,0 +1,7 @@
+# When using a virtualenv I encountered issues importing the following python
+# libraries without the dependecies found below
+
+# before pyaudio
+sudo apt-get install portaudio19-dev
+# before pygame
+sudo apt-get install python-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsdl1.2-dev libsmpeg-dev python-numpy subversion libportmidi-dev ffmpeg libswscale-dev libavformat-dev libavcodec-dev
