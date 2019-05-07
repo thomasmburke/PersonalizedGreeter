1f84628b2ce973b118b3fc0b1fb4741ce2a024d3
thomasmburke <tburke@bu.edu>
2019-05-07 12:44:51 -0400

    add friends after creation of collection

--- environment_setup/create_rekognition_collection.py
+++ environment_setup/create_rekognition_collection.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import os
 sys.path.insert(0, '../src')
 from rekognition_ops import RekognitionOps
 
@@ -10,12 +11,15 @@ if __name__ == '__main__':
         level=logging.INFO,
         format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
         datefmt='%Y-%m-%dT%H:%M:%S')
-    with open('C:/Users/thomas.burke/OneDrive - Accenture/Desktop/ThomasBurke.PNG', 'rb') as myFile:
-        # with open('C:/Users/thomas.burke/OneDrive - Accenture/Desktop/jeff.jpg', 'rb') as myFile:
-        encoded_string = myFile.read()
-    # with open('/Users/tburke/Desktop/ThomasBurke.png', 'rb') as myFile:
-        #encoded_string = base64.b64encode(myFile.read())
-        #encoded_string = myFile.read()
-    #print(RekognitionOps().add_face_to_collection('bucket', 'ThomasBurke', encoded_string))
-    # print(RekognitionOps().list_faces())
-    print(RekognitionOps().search_faces_by_image('bucket', 'photoName', encoded_string))
+    RekognitionOps().create_collection()
+    # All faces in the dataset are stored locally, so I am iterating through all of them
+    faceImages = os.listdir('/Users/tburke/Desktop/facebook_pics/')
+    for faceImage in faceImages:
+        with open('/Users/tburke/Desktop/facebook_pics/{}'.format(faceImage), 'rb') as myFile:
+            encoded_string = myFile.read()
+            RekognitionOps().add_face_to_collection(os.path.splitext(faceImage)[0], encoded_string)
+    """
+    # To clear collection of all faces
+    faceIds = RekognitionOps().get_all_face_ids()
+    print(RekognitionOps().delete_faces_from_collection(faceIds))
+    """
