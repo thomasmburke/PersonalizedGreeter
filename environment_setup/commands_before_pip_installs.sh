3d1a805fe86d46bde8201bc63e8ea2ca35ceb145
thomasmburke <tburke@bu.edu>
2019-05-08 15:39:21 -0400

    getting usb speaker to work on pi (conf)

--- environment_setup/commands_before_pip_installs.sh
+++ environment_setup/commands_before_pip_installs.sh
@@ -5,3 +5,13 @@
 sudo apt-get install portaudio19-dev
 # before pygame
 sudo apt-get install python-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsdl1.2-dev libsmpeg-dev python-numpy subversion libportmidi-dev ffmpeg libswscale-dev libavformat-dev libavcodec-dev
+
+# Setup to get usb speaker to work on card 1 of raspberry pi
+sudo vi /etc/asound.conf
+#then add the following code:
+pcm.!default  {
+ type hw card 1
+}
+ctl.!default {
+ type hw card 1
+}
