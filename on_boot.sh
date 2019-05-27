063b6022b4d84baa44e7258e18b51cdea0b1eb70
Thomas Burke <tburke@bu.edu>
2019-05-27 10:26:39 -0400

    moved logging to rotating file handler in python

--- on_boot.sh
+++ on_boot.sh
@@ -2,4 +2,4 @@
 
 source /home/pi/.profile
 workon cv
-python3 /home/pi/Desktop/PersonalizedGreeter/src/decider.py #>> /home/pi/bootlogs.txt 2>&1
+python3 /home/pi/Desktop/PersonalizedGreeter/src/decider.py
